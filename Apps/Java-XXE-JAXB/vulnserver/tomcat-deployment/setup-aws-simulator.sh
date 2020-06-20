5ada06afbc4ca82599f3380a29b158702a1668b3
hloverflow <hloverflow0@gmail.com>
2020-06-20 17:23:35 +0800

    fixed bug where network resolution fails due to iptables-legacy

--- Apps/Java-XXE-JAXB/vulnserver/tomcat-deployment/setup-aws-simulator.sh
+++ Apps/Java-XXE-JAXB/vulnserver/tomcat-deployment/setup-aws-simulator.sh
@@ -6,5 +6,6 @@ attackerip=$(ping -c 1 attackerserver-jaxb | grep "bytes from" | grep -E -o "([0
 echo $simulatorip aws-metadata-simulator-jaxb >> /etc/hosts
 echo $attackerip attackerserver-jaxb >> /etc/hosts
 
-# The following will mess up docker's container name resolution.
-iptables -t nat -A OUTPUT -p tcp -d 169.254.169.254 --dport 80 -j DNAT --to-destination $simulatorip:8111
+# The following will mess up docker's container name resolution. choose iptables-legacy if iptables --list shows warning of iptables-legacy.
+# iptables -t nat -A OUTPUT -p tcp -d 169.254.169.254 --dport 80 -j DNAT --to-destination $simulatorip:8111
+iptables-legacy -t nat -A OUTPUT -p tcp -d 169.254.169.254 --dport 80 -j DNAT --to-destination $simulatorip:8111
